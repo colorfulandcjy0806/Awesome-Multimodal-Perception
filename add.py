242c35cb8a2b72edd2dad13b201e5f1d6a18ae40
“colorfulandcjy0806” <chenjunyang0806@gmail.com>
2024-04-01 16:20:19 +0800

    add mv2d

--- add.py
+++ add.py
@@ -26,15 +26,15 @@ def insert_paper_info(entry, filepath):
 if __name__ == "__main__":
     # 示例论文条目
     new_paper = {
-        '序号': 5,
+        '序号': 6,
         '类型': '3D目标检(纯视觉)',
-        '论文名称': 'Enhancing 3D Object Detection with 2D Detection-Guided Query Anchors',
-        '作者': 'Haoxuanye Ji,Pengpeng Liang,Erkang Cheng',
-        '发表单位': '郑州大学、Nullmax',
-        '期刊/会议': 'CVPR2024',
-        '论文链接': 'https://arxiv.org/abs/2403.06093',
-        '代码链接': 'https://arxiv.org/abs/2403.06093',
-        '解读链接': 'https://zhuanlan.zhihu.com/p/688934983'
+        '论文名称': 'Object as Query: Lifting any 2D Object Detector to 3D Detection',
+        '作者': 'Zitian Wang,Zehao Huang,Jiahui Fu,Naiyan Wang,Si Liu',
+        '发表单位': '北京航空航天大学人工智能研究院、图森未来',
+        '期刊/会议': 'ICCV2023',
+        '论文链接': 'https://arxiv.org/abs/2301.02364',
+        '代码链接': 'https://github.com/tusen-ai/MV2D?tab=readme-ov-file',
+        '解读链接': 'https://zhuanlan.zhihu.com/p/690036659'
     }
 
     insert_paper_info(new_paper, "README.md")
