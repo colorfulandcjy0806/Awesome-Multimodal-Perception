92a0d6f3db2135f15040b37ed3d6411eb2820e21
“colorfulandcjy0806” <chenjunyang0806@gmail.com>
2024-04-09 01:01:07 +0800

    add MonoCD

--- add.py
+++ add.py
@@ -26,15 +26,15 @@ def insert_paper_info(entry, filepath):
 if __name__ == "__main__":
     # 示例论文条目
     new_paper = {
-        '序号': 6,
+        '序号': 7,
         '类型': '3D目标检(纯视觉)',
-        '论文名称': 'Object as Query: Lifting any 2D Object Detector to 3D Detection',
-        '作者': 'Zitian Wang,Zehao Huang,Jiahui Fu,Naiyan Wang,Si Liu',
-        '发表单位': '北京航空航天大学人工智能研究院、图森未来',
-        '期刊/会议': 'ICCV2023',
-        '论文链接': 'https://arxiv.org/abs/2301.02364',
-        '代码链接': 'https://github.com/tusen-ai/MV2D?tab=readme-ov-file',
-        '解读链接': 'https://zhuanlan.zhihu.com/p/690036659'
+        '论文名称': 'MonoCD: Monocular 3D Object Detection with Complementary Depths',
+        '作者': 'Longfei Yan, Pei Yan, Shengzhou Xiong, Xuanyu Xiang, Yihua Tan',
+        '发表单位': '华中科技大学人工智能与自动化学院',
+        '期刊/会议': 'CVPR2024',
+        '论文链接': 'https://arxiv.org/abs/2404.03181',
+        '代码链接': 'https://github.com/elvintanhust/MonoCD',
+        '解读链接': 'https://zhuanlan.zhihu.com/p/691322485'
     }
 
     insert_paper_info(new_paper, "README.md")
