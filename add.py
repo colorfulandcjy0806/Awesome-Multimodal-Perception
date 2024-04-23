60e9e265fadf9e62b7fcb421e1abf8fb1557bb48
“colorfulandcjy0806” <chenjunyang0806@gmail.com>
2024-04-23 17:44:44 +0800

    add sparse4d

--- add.py
+++ add.py
@@ -26,15 +26,15 @@ def insert_paper_info(entry, filepath):
 if __name__ == "__main__":
     # 示例论文条目
     new_paper = {
-        '序号': 9,
-        '类型': '相机+激光雷达融合',
-        '论文名称': "BEVFusion: Multi-Task Multi-Sensor Fusion with Unified Bird's-Eye View Representation",
-        '作者': 'Zhijian Liu, Haotian Tang, Alexander Amini, Xinyu Yang, Huizi Mao, Daniela Rus, Song Han',
-        '发表单位': 'MIT、上海交通大学',
-        '期刊/会议': 'ICRA 2023',
-        '论文链接': 'https://arxiv.org/abs/2205.13542',
-        '代码链接': 'https://github.com/mit-han-lab/bevfusion',
-        '解读链接': 'https://zhuanlan.zhihu.com/p/692741500'
+        '序号': 10,
+        '类型': '3D目标检测（纯视觉）',
+        '论文名称': "Sparse4D: Multi-view 3D Object Detection with Sparse Spatial-Temporal Fusion",
+        '作者': 'Xuewu Lin, Tianwei Lin, Zixiang Pei, Lichao Huang, Zhizhong Su',
+        '发表单位': '地平线',
+        '期刊/会议': '无',
+        '论文链接': 'https://arxiv.org/abs/2211.10581',
+        '代码链接': 'https://github.com/linxuewu/Sparse4D',
+        '解读链接': 'https://zhuanlan.zhihu.com/p/693836118'
     }
 
     insert_paper_info(new_paper, "README.md")
