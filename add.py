a96305be395e89ff1d570e431dec8f942fe46b71
“colorfulandcjy0806” <chenjunyang0806@gmail.com>
2024-06-01 21:50:21 +0800

    add MambaOut

--- add.py
+++ add.py
@@ -26,14 +26,14 @@ def insert_paper_info(entry, filepath):
 if __name__ == "__main__":
     # 示例论文条目
     new_paper = {
-        '序号': 16,
+        '序号': 17,
         '类型': 'Mamba架构',
-        '论文名称': "Mamba: Linear-Time Sequence Modeling with Selective State Spaces",
-        '作者': 'Lianghui Zhu, Bencheng Liao, Qian Zhang, Xinlong Wang, Wenyu Liu, Xinggang Wang',
-        '发表单位': '华中科技大学、地平线机器人、北京人工智能研究院',
+        '论文名称': "MambaOut: Do We Really Need Mamba for Vision?",
+        '作者': 'Weihao Yu, Xinchao Wang',
+        '发表单位': '新加坡国立大学',
         '期刊/会议': 'ICML2024',
-        '论文链接': 'https://arxiv.org/abs/2401.09417',
-        '代码链接': 'https://github.com/hustvl/Vim',
+        '论文链接': 'https://arxiv.org/abs/2405.07992',
+        '代码链接': 'https://github.com/yuweihao/MambaOut',
         '解读链接': 'https://zhuanlan.zhihu.com/p/700997537'
     }
 
